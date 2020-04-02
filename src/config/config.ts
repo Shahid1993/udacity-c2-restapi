3a62d837065904bf30c86a4a7fc3c608895ff4f7
Shahid1993 <shahid.bhat1994@gmail.com>
2020-04-02 09:33:09 +0530

    Deployable App completed

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,28 @@
 export const config = {
+  "postgres": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres"
+  },
+  "aws": {
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  }
+}
+
+/* export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramshahiddev",
+    "password": "udagramshahiddev",
+    "database": "udagramshahiddev",
+    "host": "udagramshahiddev.czfmcr69pfg3.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": "us-east-1",
+    "aws_profile": "udagram-dev",
+    "aws_media_bucket": "udagram-shahid-dev"
   },
   "prod": {
     "username": "",
@@ -16,4 +31,4 @@ export const config = {
     "host": "",
     "dialect": "postgres"
   }
-}
+} */
